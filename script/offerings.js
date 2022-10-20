7f3f85207ef3779f3a919392d724a3c5e04def0e
HardikShahh563 <hardikts@gmail.com>
2022-10-20 14:06:43 +0530

    made changes

--- script/offerings.js
+++ script/offerings.js
@@ -1,14 +1,3 @@
-var plus = document.querySelectorAll(".plus");
-
-function plusclick() {
-    plus.forEach(pl => {
-        pl.addEventListener("click", () => {
-            pl.classList.toggle("active");
-        });
-    });
-}
-setInterval(plusclick(), 1);
-
 var addTocart = document.querySelectorAll(".add-to-cart");
 var addTocartOverlay = document.querySelector(".add-to-cart-overlay");
 function addToCartClick() {
